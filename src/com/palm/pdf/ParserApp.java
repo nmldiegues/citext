4f7384e3e12c2a7b6f0c6fe209f4b442eda5a725
Nuno Diegues <nmld@ist.utl.pt>
2011-07-09 12:41:01 +0100

    Dealt with more variants of citations envolving [1-4] [1,2,3] [1-4, 6, 8-10].

--- src/com/palm/pdf/ParserApp.java
+++ src/com/palm/pdf/ParserApp.java
@@ -1,29 +1,38 @@
 package com.palm.pdf;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
 
 /*
- * TODO deal with [1-4] [1, 4, 2]
  * TODO fetch the actual citation to the bibtex
  * TODO parse the authors and paper name from the bibtex
+ * TODO ant build file. move articles to resources. create jar
+ * TODO refactor to proper classes
  */
 public class ParserApp {
 
 	private static int lastIdxParsed = 0;
 
 	public static void main(String[] args) throws Exception {
-		String str = readPdf("article.pdf");
+		String str = readPdf("article2.pdf");
 
-		Integer bibNum9 = findCitation(str);
-		System.out.println(retrieveBibtex(str, bibNum9));
+		helperPrinter(str, findCitation(str));
+		helperPrinter(str, findCitation(str));
+		helperPrinter(str, findCitation(str));
+		helperPrinter(str, findCitation(str));
+		helperPrinter(str, findCitation(str));
+	}
 
-		Integer bibNum1 = findCitation(str);
-		System.out.println(retrieveBibtex(str, bibNum1));
-
+	public static void helperPrinter(String article, List<Integer> bibNums) {
+		for (Integer bibNum : bibNums) {
+			System.out.println(retrieveBibtex(article, bibNum));
+		}
 	}
 
 	public static String readPdf(String fileName) throws IOException {
@@ -50,20 +59,103 @@ public class ParserApp {
 
 	public static boolean isCitation(String input) {
 		try {
-			Integer.parseInt(input);
+			Integer.parseInt(input.trim());
 		} catch (NumberFormatException e) {
 			return false;
 		}
 		return true;
 	}
 
-	public static Integer findCitation(String input) throws NoMoreCitationException {
+	public static boolean isMultipleCitation(String input, String separator) {
+		for (String str : input.split(separator)) {
+			if (!isCitation(str)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isMultipleCitationNested(String input, String outsideSep, String nestedSep) {
+		for (String str : input.split(outsideSep)) {
+			if (!isMultipleCitation(str, nestedSep)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Produces a list of numbers in the next citation found in the article.
+	 * Supports the following types: [1] [1-3] [1, 4, 7] [1-3, 5, 8, 13-17, 21]
+	 * With the following possible hyphens: - (45) – (150) — (151)
+	 * 
+	 * @param input
+	 *            The article text.
+	 * @return A list containing all the numbers in the next citation.
+	 * @throws NoMoreCitationException
+	 *             If there are no more citations in the text, given the
+	 *             internal state kept regarding the cursor in the article.
+	 */
+	public static List<Integer> findCitation(String input) throws NoMoreCitationException {
+		String[] separators = { "-", "–", "—" };
+
 		while (true) {
 			String result = filterStringSequence(findCitationAux(input), "-\n", "-", "\n");
 			if (isCitation(result)) {
-				return Integer.parseInt(result);
+				return Collections.singletonList(Integer.parseInt(result));
+			}
+
+			for (String sep : separators) {
+				if (isMultipleCitationNested(result, ",", sep)) {
+					List<Integer> listResult = new ArrayList<Integer>();
+					for (String str : result.split(",")) {
+						if (str.contains(sep)) {
+							listResult.addAll(batchCitationNumbers(str, sep));
+						} else {
+							listResult.add(Integer.parseInt(str.trim()));
+						}
+					}
+					return listResult;
+				}
+			}
+
+			if (isMultipleCitation(result, ",")) {
+				List<Integer> listResult = new ArrayList<Integer>();
+				for (String str : result.split(",")) {
+					listResult.add(Integer.parseInt(str.trim()));
+				}
+				return listResult;
+			}
+
+			for (String sep : separators) {
+				if (isMultipleCitation(result, sep)) {
+					return batchCitationNumbers(result, sep);
+				}
 			}
 		}
+	}
+
+	/**
+	 * Given a citation containing interval of numbers, returns a list of all
+	 * the numbers.
+	 * 
+	 * @param input
+	 *            Assumes that the input consists of something like 1-4.
+	 * @param separator
+	 *            The actual separator of the number interval provided.
+	 * @return List containing all integers given in the input interval.
+	 */
+	private static List<Integer> batchCitationNumbers(String input, String separator) {
+		String[] bounds = input.split(separator);
+		Integer lowerBound = Integer.parseInt(bounds[0].trim());
+		Integer upperBound = Integer.parseInt(bounds[1].trim());
+		List<Integer> listResult = new ArrayList<Integer>();
+		listResult.add(lowerBound);
+		for (int i = lowerBound + 1; i < upperBound; i++) {
+			listResult.add(i);
+		}
+		listResult.add(upperBound);
+		return listResult;
 	}
 
 	private static String findCitationAux(String input) throws NoMoreCitationException {
